eb257f7bdbaec7d8b78063980fedb845c59340b1
brandon <natakumon2@yahoo.com>
2023-03-13 14:00:31 -0700

    added arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,80 @@
 // Assignment code here
+//between 8 and 128 characters
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var specialArr = ["@", "%", "+", "\\", "/", "'", "!", "#", "$",  "^", "?", ":", ",", ")",  "(",  "}",  "{",  "]", "[", "~", "-", "_", ".",];
+
+var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var lowerArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var upperArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var lengthOfPassword = window.prompt("Enter Password Length: ")
 
 // Write password to the #password input
 function writePassword() {
