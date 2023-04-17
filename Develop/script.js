aceaf4b8461975152217fa476aa221d8ce623e51
brandon <natakumon2@yahoo.com>
2023-04-17 14:13:38 -0700

    added concat ability

--- Develop/script.js
+++ Develop/script.js
@@ -12,7 +12,7 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var symbolArr = ["@", "%", "+", "\\", "/", "'", "!", "#", "$",  "^", "?", ":", ")",  "(",  "}", ",",  "{",  "]", "[", "~", "-", "_", ".",];
+var symbolArr = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ")", "(", "}", ",", "{", "]", "[", "~", "-", "_", ".",];
 
 var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -74,81 +74,71 @@ var upperArr = [
   "Z",
 ];
 
-var generatePassword = (length,hasNumbers, hasSymbols, hasLower, hasUpper)=>{
-  var userArr = [
-    (hasNumbers ? numberArr : []),
-    (hasSymbols ? symbolArr : []),
-    (hasLower ? lowerArr : []),
-    (hasUpper ? symbolArr : []),    
-  ]
-} // check? added last
 
-var lengthOfPassword = parseInt(window.prompt("Enter Password Length: "));
-//var symbolOfPassword = window.Boolean ("Do you want symbols?")
-var numberChoice= window.prompt("Do you want numbers: 'Yes' or 'No'");
-var symbolChoice= window.prompt("Do you want symbols: 'Yes' or 'No'");
-var lowerChoice= window.prompt("Do you want lower case letters: 'Yes' or 'No'");
-var upperChoice= window.prompt("Do you want upper case letters: 'Yes' or 'No'");
-
-
-while (lengthOfPassword<8|| lengthOfPassword> 128) {
-  window.alert("Please enter a password length value of between 8 and 128");
-  lengthOfPassword=parseInt(window.prompt("Enter Password Length: ")); //causes program to cycle
-}
-
-if (numberChoice=="Yes") {
-  window.alert("Confirmed:Using numbers.")
-} else {
-  window.alert("Confirmed:No numbers")
-}
-
-if (symbolChoice=="Yes") {
-  window.alert("Confirmed:Using symbols.")
-} else {
-  window.alert("Confirmed:No symbols")
-}
-
-if (lowerChoice=="Yes") {
-  window.alert("Confirmed:Using lower letters.")
-} else {
-  window.alert("Confirmed:No lower letters")
-}
-
-if (upperChoice=="Yes") {
-  window.alert("Confirmed:Using upper letters.")
-} else {
-  window.alert("Confirmed:No upper letters")
-}
-
-//notes below
-
-var result = [];
-
-for (var i =0; i < lengthOfPassword; i++) {
-  //var randomIndex=Math.floor(Math.random()*numberArr.length);// this or the following, both arent needed
-  //var randomNum = numberArr[Math.floor(Math.random()*numberArr.length)];// summarized all of the above
-  
-  //if (availableCharacters.length===0) return "";
+var generatePassword = () => {
+  var userArr = [];
 
 
 
-  if (numberChoice=="Yes" && symbolChoice=="Yes" && lowerChoice=="Yes" && upperChoice=="Yes"){
-    var randomIndex=userArr[Math.floor(Math.random()*(userArr.length))];
-  } else {
-    var randomIndex=symbolArr[Math.floor(Math.random()*symbolArr.length)];// removed "," from symbols to avoid confusion
+  var lengthOfPassword = parseInt(window.prompt("Enter Password Length: "));
+  while (lengthOfPassword < 8 || lengthOfPassword > 128) {
+    window.alert("Please enter a password length value of between 8 and 128");
+    lengthOfPassword = parseInt(window.prompt("Enter Password Length: ")); //causes program to cycle
   }
-  result.push(randomIndex);
-  console.log(result); 
+  //var symbolOfPassword = window.Boolean ("Do you want symbols?")
+  var numberChoice = window.prompt("Do you want numbers: 'Yes' or 'No'");
+  if (numberChoice == "Yes") {
+    userArr = userArr.concat(numberArr);
+    window.alert("Confirmed:Using numbers.")
+  } else {
+    window.alert("Confirmed:No numbers")
+  }
+
+  var symbolChoice = window.prompt("Do you want symbols: 'Yes' or 'No'");
+  if (symbolChoice == "Yes") {
+    userArr = userArr.concat(symbolArr);
+    window.alert("Confirmed:Using symbols.")
+  } else {
+    window.alert("Confirmed:No symbols")
+  }
+
+  var lowerChoice = window.prompt("Do you want lower case letters: 'Yes' or 'No'");
+  if (lowerChoice == "Yes") {
+    userArr = userArr.concat(lowerArr);
+    window.alert("Confirmed:Using lower letters.")
+  } else {
+    window.alert("Confirmed:No lower letters")
+  }
+
+  var upperChoice = window.prompt("Do you want upper case letters: 'Yes' or 'No'");
+  if (upperChoice == "Yes") {
+    userArr = userArr.concat(upperArr);
+    window.alert("Confirmed:Using upper letters.")
+  } else {
+    window.alert("Confirmed:No upper letters")
+  }
+
+  //notes below
+
+  var result = [];
+
+  for (var i = 0; i < lengthOfPassword; i++) {
+
+    var randomIndex = userArr[Math.floor(Math.random() * (userArr.length))];
+
+    result.push(randomIndex);
+    //  console.log(result); 
+  }
+
+  console.log(result.join("")); // join combines the password so theres no , between each unit
+  // Write password to the #password input
+  return result.join("");
 }
-
-console.log(result.join("")); // join combines the password so theres no , between each unit
-// Write password to the #password input
 function writePassword() {
-  password.textContent = (result.join(""));
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  //passwordText.value = password;
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
